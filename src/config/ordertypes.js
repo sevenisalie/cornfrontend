02d6347beebcdd2496fcd20dd547e4d80c8eb32d
sevenisalie <nolan.e.terry@gmail.com>
2022-04-11 23:25:21 -0500

    mobile ios fix .at()

--- src/config/ordertypes.js
+++ src/config/ordertypes.js
@@ -6,28 +6,28 @@ export const ORDERTYPES = [
     {   
         pid: 0,
         name: "Limit",
-        address: ADDRESSES["137"]["Controller"].at(0),
+        address: ADDRESSES["137"]["Controller"][0],
         tickets: 1,
         image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
     },
     {   
         pid: 1,
         name: "Stop",
-        address: ADDRESSES["137"]["Controller"].at(0),
+        address: ADDRESSES["137"]["Controller"][0],
         tickets: 1,
         image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
     },
     {   
         pid: 2,
         name: "Accumulator Distributor",
-        address: ADDRESSES["137"]["Controller"].at(0),
+        address: ADDRESSES["137"]["Controller"][0],
         tickets: 2,
         image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
     },
     {   
         pid: 3,
         name: "OCO Bracket",
-        address: ADDRESSES["137"]["Controller"].at(0),
+        address: ADDRESSES["137"]["Controller"][0],
         tickets: 2,
         image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
     }
