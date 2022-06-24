576e7d558d935ede35feb7d755c7721207baa3f7
Korey Corkscrew <cameronbiniamow@Camerons-MacBook-Pro.local>
2022-06-24 12:25:31 -0500

    merged

--- src/config/vaults.js
+++ src/config/vaults.js
@@ -3,20 +3,20 @@ import {addresses} from "./addresses"
 import ADDRESSES from "./build/deployments/map.json"
 
 export const VAULTS = [
-    // {   
-    //     pid: 0,
-    //     name: "OLD-Limit",
-    //     address: "0x44C1e896Db557922230C042D4c577b173EE51472",
-    //     tickets: 1,
-    //     image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
-    // },
-    // {   
-    //     pid: 1,
-    //     name: "OLD-LIMIT",
-    //     address: "0xbCaDa3b2c493447E655eC08D0f79864baa862120",
-    //     tickets: 1,
-    //     image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
-    // },
+    {   
+        pid: 0,
+        name: "OLD-Limit",
+        address: "0x44C1e896Db557922230C042D4c577b173EE51472",
+        tickets: 1,
+        image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
+    },
+    {   
+        pid: 1,
+        name: "OLD-LIMIT",
+        address: "0xbCaDa3b2c493447E655eC08D0f79864baa862120",
+        tickets: 1,
+        image: "https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png",
+    },
     {   
         pid: 2,
         name: "LIMIT",
